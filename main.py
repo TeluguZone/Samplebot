bc2105803c01d51c64e44fef7b33a8a532825d3b
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-12-10 14:49:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from pyrogram import Client,filters
 
+
+API_ID = ""
+API_HASH = ""
+BOT_TOKEN = ""
+
+TeluguZone = Client(
+    name="Samplebot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+  
